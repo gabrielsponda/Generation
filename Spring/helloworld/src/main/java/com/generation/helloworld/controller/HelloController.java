3f2c85e61eb23e78aada7b3fbf504837f5799ec7
Gabriel Sponda Freitas Bettarello <sponda.gabriel@aluno.ufabc.edu.br>
2023-12-07 14:00:35 -0300

    Modificando o projeto para atender aos requisitos do Canvas.

--- Spring/helloworld/src/main/java/com/generation/helloworld/controller/HelloController.java
+++ Spring/helloworld/src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -10,11 +10,32 @@ public class HelloController {
 
 	@GetMapping
 	public String hello() {
-		return "<h1>Hello, World!</h1>";
+		return "<h1>Hello, World!</h1>" +
+			       "<a href='http://localhost:8080/hello/bsm'><b>BSM</b></a>" +
+			       "<br>" +
+			       "<a href='http://localhost:8080/hello/objetivos'><b>Objetivos</b></a>";
 	}
-	
-	@GetMapping("/turma")
-	public String turma() {
-		return "<b>Turma 68!</b>";
+
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<ul>" +
+		        "<li>Orientação ao futuro</li>" +
+		        "<li>Mentalidade de Crescimento</li>" +
+		        "<li>Persistência</li>" +
+		        "<li>Responsabilidade Pessoal</li>" +
+		        "<li>Comunicação</li>" +
+		        "<li>Proatividade</li>" +
+		        "<li>Orientação ao detalhe</li>" +
+		        "<li>Trabalho em equipe</li>" +
+		        "</ul>";
+	}
+
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<ul>" +
+		        "<li>Dominar a estrutura básica do HTML</li>" +
+		        "<li>Criar links e âncoras</li>" +
+		        "<li>Formatar texto e conteúdo</li>" +
+		        "</ul>";
 	}
 }
